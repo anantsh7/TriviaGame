a973d2e4f53c0d9c7b9c5514e43b4b9cdb92cee0
Anant Sharma <anantsh7@gmail.com>
2018-09-20 17:51:33 -0400

    some js added

---
+++ assets/javascript/app.js
@@ -0,0 +1,60 @@
+// Questions for game
+var questions = [{
+    q: "A Boeing 777 has how many engines?",
+    a: ["2", "7", "3", "1"],
+    correct_a: "2"
+  },
+  {
+    q: "What U.S. agency’s motto is “Fidelity, Bravery, Integrity”?",
+    a: ["CIA", "FBI", "NSA", "CDC"],
+    correct_a: "FBI"
+  },
+  {
+    q: "The live action superhero television series “Mighty Morphin Power Rangers” premiered in what year?",
+    a: ["1994", "1997", "1993", "2000"],
+    correct_a: "1993"
+  },
+  {
+    q: "Able to be seen from outer space, what is Earth’s largest living structure?",
+    a: ["Great Wall of China", "Eiffel Tower", "Great Barrier Reef", "Amazon Forest"],
+    correct_a: "Great Barrier Reef"
+  },
+  {
+    q: "On which popular website do users send tweets?",
+    a: ["Facebook", "Twitter", "Snapchat", "Instagram"],
+    correct_a: "Twitter"
+  },
+  {
+    q: "What was the first console video game that allowed the game to be saved?",
+    a: ["The Legend of Zelda", "Rolling Thunder", "Metroid", "Adventure Island"],
+    correct_a: "The Legend of Zelda"
+  },
+  {
+    q: "With twelve Oscar nominations and three wins, who is the most nominated male actor in Academy Awards history?",
+    a: ["Jack Nicholson", "Christian Bale", "Leonardo Dicaprio", "Mel Gibson"],
+    correct_a: "Jack Nicholson"
+  },
+  {
+    q: "“Michael Keaton played which Spiderman villain in the 2017 superhero movie “Spiderman: Homecoming”?”",
+    a: ["Rhino", "Dr.Octavious", "Vulture", "Green Goblin"],
+    correct_a: "2"
+  },
+  {
+    q: "How many teams are in the American National Football league?",
+    a: ["16", "24", "20", "32"],
+    correct_a: "2"
+  },
+  {
+    q: "Professional footballer Lionel Messi was born in which country?",
+    a: ["U.S.A", "Spain", "Argentina", "Portugal"],
+    correct_a: "Argentina"
+  },
+]
+
+// Variable to keep track of game data
+
+var correct = 0;
+var incorrect = 0;
+var time = 15; 
+var questions_missed = 0;
+
